01ff10cf1c6cfa4b9f8c6e70bb24fb355a0fdb40
Malvado996 <malvado996@gmail.com>
2021-03-15 20:27:19 -0400

    connected to Mongo DB

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB()
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
@@ -18,3 +28,10 @@ app.listen(
     PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`ERROR: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+})
