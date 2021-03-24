8c8cf814d00a6e37a0005287c806eaa4ddee94eb
Malvado996 <malvado996@gmail.com>
2021-03-24 17:00:22 -0400

    basic CRUD operations done with try/catch blocks

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 // Dev logging middleware
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -31,7 +34,7 @@ const server = app.listen(PORT,
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-    console.log('Unhandled Error from server.js: ${err.message}'.red);
+    console.log(`Unhandled Error from server.js: ${err.message}`.red.bold.underline);
     // Close server & exit process
     server.close(() => process.exit(1))
 })
