77235de33441dcfa514b593c9540486825543c76
Malvado996 <malvado996@gmail.com>
2021-06-18 21:37:06 -0400

    commit

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -94,29 +94,3 @@ exports.getBootcampsInRadius = asyncHandler(async ( req, res, next ) => {
     })
 });
 
-// // @desc    Get Bootcamps within a radius
-// // @route   GET /api/v1/bootcamps/radius/:zipcode/:distance
-// // @access  Private
-// exports.getBootcampsInRadius = asyncHandler(async ( req, res, next ) => {
-//     const { zipcode, distance } = req.params;
-
-//     // Get lat/lng from geocoder
-//     const loc = await geocoder.geocode(zipcode);
-//     const lat = loc[0].latitude;
-//     const lng = loc[0].longitude;
-
-//     //  Calc radius using radians
-//     //  Divide dist by radius of Earth
-//     //  Earth radius = 3,963 mi
-//     const radius = distance / 3963;
-
-//     const bootcamps = await Bootcamp.find({
-//         location: { $geoWithin: { $centerSphere: [ [ lng, lat, ], radius ] } }
-//     });
-
-//     res.status(200).json({
-//         success: true,
-//         count: bootcamps.length,
-//         data: bootcamps
-//     })
-// });
